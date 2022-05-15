4e24748bb9711b8c937f54a64c929704587e7c56
coding-sta <personal.sta.bd@gmail.com>
2022-05-15 06:06:41 +0600

    database include+ appoint api

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const express = require('express');
-const app = express()
-const port=process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>{
-res.send('hello')
-})
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-app.listen(port,()=>{
-    console.log(`hello from ${port}`)
-})
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q9cpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    client.connect();
+    const appointmentCollection = client
+      .db("doctors_portal")
+      .collection("appointments");
+
+    app.get("/appointment", async (req, res) => {
+      const query = {};
+      const cursor = appointmentCollection.find(query);
+      const results=await cursor.toArray()
+      res.send(results)
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log(`port ${port}`);
+});
